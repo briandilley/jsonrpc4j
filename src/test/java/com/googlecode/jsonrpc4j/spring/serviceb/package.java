3c90ead8738ceddd573576eed325a650b5171768
Andrew Lindesay <apl@lindesay.co.nz>
2016-03-15 08:19:16 +1300

    Adjust Auto Exporter Approach to Constrain Exports to Specific Implementations

---
+++ src/test/java/com/googlecode/jsonrpc4j/spring/serviceb/package.java
@@ -0,0 +1,8 @@
+/**
+ * <p>This set of service classes is designed to test the
+ * {@link com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceImplExporter} bean used to
+ * help with exposing JSON-RPC services.
+ * </p>
+ */
+
+package com.googlecode.jsonrpc4j.spring.serviceb;
