1c1b8991e2fcc208bd1c0b84d248d749f3f408fd
cyb3r4nt <cyb3r4nt@github.com>
2022-06-11 02:28:30 +0300

    #290: Fix JSON-RPC id output in case of method type conversion errors.
    Server used to output null as id value if some parameter conversion has been failed.
    Request id context was lost due to exceptions and null was sent to client.
    
    Fix contains more grained exception handling and restore of initial request context.
    
    Added also tests, which check for expected id result in the responses.

--- src/test/java/com/googlecode/jsonrpc4j/server/JsonRpcServerBatchTest.java
+++ src/test/java/com/googlecode/jsonrpc4j/server/JsonRpcServerBatchTest.java
@@ -3,6 +3,9 @@ package com.googlecode.jsonrpc4j.server;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.googlecode.jsonrpc4j.JsonRpcServer;
+import com.googlecode.jsonrpc4j.server.JsonRpcServerAnnotatedParamTest.ServiceInterfaceWithParamNameAnnotation;
+import com.googlecode.jsonrpc4j.util.Util;
+
 import org.easymock.EasyMock;
 import org.easymock.Mock;
 import org.easymock.MockType;
@@ -12,19 +15,18 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.util.StreamUtils;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
 
-import static com.googlecode.jsonrpc4j.JsonRpcBasicServer.DATA;
-import static com.googlecode.jsonrpc4j.JsonRpcBasicServer.ERROR;
-import static com.googlecode.jsonrpc4j.JsonRpcBasicServer.ERROR_MESSAGE;
-import static com.googlecode.jsonrpc4j.JsonRpcBasicServer.RESULT;
-import static com.googlecode.jsonrpc4j.util.Util.decodeAnswer;
-import static com.googlecode.jsonrpc4j.util.Util.getFromArrayWithId;
-import static com.googlecode.jsonrpc4j.util.Util.messageWithListParams;
-import static com.googlecode.jsonrpc4j.util.Util.multiMessageOfStream;
-import static com.googlecode.jsonrpc4j.util.Util.toByteArrayOutputStream;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static com.googlecode.jsonrpc4j.ErrorResolver.JsonError.METHOD_PARAMS_INVALID;
+import static com.googlecode.jsonrpc4j.JsonRpcBasicServer.*;
+import static com.googlecode.jsonrpc4j.server.JsonRpcServerAnnotatedParamTest.METHOD_WITH_DIFFERENT_TYPES;
+import static com.googlecode.jsonrpc4j.util.Util.*;
+import static org.junit.Assert.*;
 
 public abstract class JsonRpcServerBatchTest {
     @Mock(type = MockType.NICE)
@@ -41,12 +43,7 @@ public abstract class JsonRpcServerBatchTest {
                 messageWithListParams(1, "testMethod", "Parameter1"),
                 messageWithListParams(2, "testMethod", "Parameter2"));
 
-        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/test-post");
-        MockHttpServletResponse response = new MockHttpServletResponse();
-
-        request.setContent(StreamUtils.copyToByteArray(inputStream));
-
-        jsonRpcServer.handle(request, response);
+        MockHttpServletResponse response = handleRequest(inputStream);
 
         assertEquals(HttpServletResponse.SC_OK, response.getStatus());
         JsonNode answer = decodeAnswer(toByteArrayOutputStream(response.getContentAsByteArray()));
@@ -65,17 +62,100 @@ public abstract class JsonRpcServerBatchTest {
                 messageWithListParams(1, "testMethod", "Parameter1"),
                 messageWithListParams(2, "testMethod", "Parameter2"));
 
-        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/test-post");
-        MockHttpServletResponse response = new MockHttpServletResponse();
-
-        request.setContent(StreamUtils.copyToByteArray(inputStream));
-
-        jsonRpcServer.handle(request, response);
+        MockHttpServletResponse response = handleRequest(inputStream);
 
         assertEquals(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, response.getStatus());
         JsonNode answer = decodeAnswer(toByteArrayOutputStream(response.getContentAsByteArray()));
         assertTrue(answer instanceof ArrayNode);
         assertEquals("Error", getFromArrayWithId(answer, 1).get(ERROR).get(DATA).get(ERROR_MESSAGE).asText());
         assertEquals("Result2", getFromArrayWithId(answer, 2).get(RESULT).asText());
+    }
+
+    @Test
+    public void parallelBatchWithInvalidRequestInsideShouldPreserveJsonRpcId() throws Exception {
+        ServiceInterfaceWithParamNameAnnotation mockServiceWithParams =
+            EasyMock.strictMock(ServiceInterfaceWithParamNameAnnotation.class);
+
+        jsonRpcServer = new JsonRpcServer(
+            Util.mapper,
+            mockServiceWithParams,
+            ServiceInterfaceWithParamNameAnnotation.class
+        );
+
+        final String validResult = "passResult";
+        final String validResultOne = validResult + "1";
+        final String validResultThree = validResult + "3";
+
+        EasyMock
+            .expect(
+                mockServiceWithParams.methodWithDifferentTypes(
+                    EasyMock.anyBoolean(),
+                    EasyMock.anyDouble(),
+                    EasyMock.anyObject(UUID.class)
+                )
+            )
+            .andReturn(validResultOne);
+        EasyMock
+            .expect(
+                mockServiceWithParams.methodWithDifferentTypes(
+                    EasyMock.anyBoolean(),
+                    EasyMock.anyDouble(),
+                    EasyMock.anyObject(UUID.class)
+                )
+            )
+            .andReturn(validResultThree);
+        EasyMock.replay(mockServiceWithParams);
+
+        final List<Object> validParams = Arrays.asList(true, 3.14, UUID.randomUUID());
+        final String invalidBoolean = "truth";
+
+        InputStream inputStream = multiMessageOfStream(
+            messageOfStream(1, METHOD_WITH_DIFFERENT_TYPES, validParams),
+            messageOfStream(
+                2,
+                METHOD_WITH_DIFFERENT_TYPES,
+                Arrays.asList(invalidBoolean, 3.14, UUID.randomUUID())
+            ),
+            messageOfStream(3, METHOD_WITH_DIFFERENT_TYPES, validParams)
+        );
+
+        MockHttpServletResponse response = handleRequest(inputStream);
+
+        EasyMock.verify(mockServiceWithParams);
+
+        assertEquals(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, response.getStatus());
+        JsonNode answer = decodeAnswer(toByteArrayOutputStream(response.getContentAsByteArray()));
+        assertTrue(answer instanceof ArrayNode);
+
+        assertEquals(validResultOne, getFromArrayWithId(answer, 1).get(RESULT).asText());
+        assertEquals(validResultThree, getFromArrayWithId(answer, 3).get(RESULT).asText());
+
+        JsonNode secondResponse = getFromArrayWithId(answer, 2);
+        JsonNode responseId = secondResponse.get(ID);
+        assertNotNull(responseId);
+        assertTrue(responseId.isInt());
+        assertEquals(2, responseId.asInt());
+
+        JsonNode error = secondResponse.get(ERROR);
+        assertNotNull(error);
+
+        assertEquals(METHOD_PARAMS_INVALID.code, errorCode(error).asInt());
+
+        String errorMessage = error.get(ERROR_MESSAGE).asText();
+        assertTrue(errorMessage.toLowerCase(Locale.ROOT).contains("index"));
+        assertTrue(errorMessage.contains("0"));
+    }
+
+    private static MockHttpServletRequest createRequest(InputStream inputStream) throws IOException {
+        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/test-post");
+        request.setContent(StreamUtils.copyToByteArray(inputStream));
+        return request;
+    }
+
+    private MockHttpServletResponse handleRequest(InputStream inputStream) throws IOException {
+        MockHttpServletRequest request = createRequest(inputStream);
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        jsonRpcServer.handle(request, response);
+        return response;
     }
 }
