a470bc935d0e8b1c2e99ed682dd2523937e77d7f
Ondřej Fibich <ondrej.fibich@gmail.com>
2022-10-18 14:26:20 +0200

    Upgrade dependencies to latest Resolves CVEs from Spring Core, Spring Web MVC and Jackson Databind.

--- src/test/java/com/googlecode/jsonrpc4j/integration/HttpCodeTest.java
+++ src/test/java/com/googlecode/jsonrpc4j/integration/HttpCodeTest.java
@@ -18,25 +18,26 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.startsWith;
 
 public class HttpCodeTest extends BaseRestTest {
-	
+
 	@Test
 	public void http405OnInvalidUrl() throws MalformedURLException {
 		expectedEx.expectMessage(anyOf(
 				equalTo("405 HTTP method POST is not supported by this URL"),
 				equalTo("404 Not Found"),
 				equalTo("HTTP method POST is not supported by this URL"),
+                                equalTo("Method Not Allowed"),
 				startsWith("Server returned HTTP response code: 405 for URL: http://127.0.0.1")));
 		expectedEx.expect(Exception.class);
 		FakeServiceInterface service = ProxyUtil.createClientProxy(FakeServiceInterface.class, getClient("error"));
 		service.doSomething();
 	}
-	
+
 	@Test
 	public void http200() throws MalformedURLException {
 		FakeServiceInterface service = ProxyUtil.createClientProxy(FakeServiceInterface.class, getClient());
 		service.doSomething();
 	}
-	
+
 	@Test
 	public void httpCustomStatus() throws MalformedURLException {
 		expectedEx.expectMessage(equalTo("Server Error"));
