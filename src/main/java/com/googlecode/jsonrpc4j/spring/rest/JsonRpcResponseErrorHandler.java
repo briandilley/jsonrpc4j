5ffb288c43ffc4635c525f272611f3fca4678d63
Shao-Chun Ma <shao.ma@lendico.de>
2023-12-08 13:02:53 +0100

    4592320: upgrade to Spring Boot 3

--- src/main/java/com/googlecode/jsonrpc4j/spring/rest/JsonRpcResponseErrorHandler.java
+++ src/main/java/com/googlecode/jsonrpc4j/spring/rest/JsonRpcResponseErrorHandler.java
@@ -7,7 +7,11 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.client.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.UnknownHttpStatusCodeException;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -71,7 +75,7 @@ public class JsonRpcResponseErrorHandler
 		try {
 			statusCode = response.getStatusCode();
 		} catch (IllegalArgumentException ex) {
-			throw new UnknownHttpStatusCodeException(response.getRawStatusCode(),
+			throw new UnknownHttpStatusCodeException(response.getStatusCode().value(),
 					response.getStatusText(), response.getHeaders(), getResponseBody(response), getCharset(response));
 		}
 		return statusCode;
